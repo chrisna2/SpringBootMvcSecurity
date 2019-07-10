7043f9b0aa452ac9ae62b833280858203fa8f3c9
MOBISEC <mobisec.sales@gmail.com>
2019-07-10 21:50:04 +0900

    jpa 맛보기 완료 ㅜㅜ 언제 시큐리티

---
+++ src/main/java/com/tyn/boot/entity/Account.java
@@ -0,0 +1,77 @@
+package com.tyn.boot.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+	
+	@Id // 아이디
+	@GeneratedValue//값을 자동으로 증가 시켜 생성
+	private Long id;
+	
+	@Column(unique=true)
+	private String username;
+	
+	@Column
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	
+}
---
+++ src/test/java/com/tyn/boot/repository/AccountRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.tyn.boot.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tyn.boot.entity.Account;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest //application.class 모든 것을 읽는다.
+//@DataJpaTest //슬라이스 테스트
+public class AccountRepositoryTest {
+
+	@Autowired
+	AccountRepository accountRepository;
+	
+	/*
+	@Autowired
+	DataSource dataSource;
+	
+	@Test
+	public void conn() throws SQLException {
+		try(Connection conn = dataSource.getConnection()){
+			System.out.println("==============|DataBaseRunner|=============");
+			System.out.println("URL : "+conn.getMetaData().getURL());
+			System.out.println("UserName : "+conn.getMetaData().getUserName());
+			
+			//mariadb로 데이터 소스ㄹ를 변경 하였을 경우 주석처리 하실 것!
+			//Statement statement = conn.createStatement();
+			//String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, NAME VARCHAR(255), PRIMARY KEY(ID))";
+			//statement.executeUpdate(sql);
+			//jdbcTemplate.execute("INSERT INTO USER VALUES(1,'HYUNKEENA')");
+		}
+	}
+	*/
+	
+	@Test
+	public void account() throws Exception {
+		
+		Account account = new Account();
+		
+		account.setUsername("hyunkee");
+		account.setPassword("1111");
+		
+		//등록! create : DB날리고 다시 입력하니 괜춘 | update : 있는 데이터를 또 입력하니 안괜춘... 듀플리케잇트
+		Account newAcc = accountRepository.save(account);
+		System.out.println(newAcc.getId()+" "+newAcc.getUsername());
+		assertThat(newAcc).isNotNull();
+		
+		//조회!
+		Account existAcc = accountRepository.findByUsername(newAcc.getUsername());
+		assertThat(existAcc).isNotNull();
+
+		//없는 것을 조회!
+		Account notExistAcc = accountRepository.findByUsername("test");
+		assertThat(notExistAcc).isNull();
+	}
+}
