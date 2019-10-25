ea25d6300209d483177b96388cc3a1fbc299eb63
chrisna2 <marksman703@gmail.com>
2019-10-25 15:18:30 +0900

    JPA테스트 완료

--- src/test/java/com/tyn/boot/repository/AccountRepositoryTest.java
+++ src/test/java/com/tyn/boot/repository/AccountRepositoryTest.java
@@ -5,8 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ import com.tyn.boot.entitiy.Account;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)//이렇게 되면 이름이 역순으로 실행 된다.
 public class AccountRepositoryTest {
 	
 	Logger log = LoggerFactory.getLogger(AccountRepositoryTest.class);
@@ -25,29 +28,10 @@ public class AccountRepositoryTest {
 	AccountRepository accountRepository;
 	
 	@Test
-	public void account() throws Exception{
-		Account account = new Account();
-		account.setUsername("chrisna2");
-		account.setPassword("pass");
-		account.setEmail("chrisna2@bans.com");
-		Account newAcct = accountRepository.save(account);
-		log.info(newAcct.getId()+" / "+newAcct.getUsername());
-		assertThat(newAcct).isNotNull();
-		
-		Optional<Account> existAcct = accountRepository.findByUsername(newAcct.getUsername());
-		assertThat(existAcct).isNotEmpty();
-		
-		Optional<Account> notExistAcct = accountRepository.findByUsername("test");
-		assertThat(notExistAcct).isEmpty();
-		
-	}
-	
-	@Test
-	public void inspect() {
+	public void test1Inspect() {
 		
 		//실제 객체의 클래스 이름
 		Class<?> clz = accountRepository.getClass();
-		
 		log.info(clz.getName());
 		
 		//클래스가 구현하고 있는 인터페이서 목록
@@ -57,7 +41,63 @@ public class AccountRepositoryTest {
 		//클래스의 부모 클래스
 		Class<?> superClasses = clz.getSuperclass();
 		log.info(superClasses.getName());
-		
 
 	}
+	
+	@Test
+	public void test2Insert() {
+		Account account = new Account();
+		account.setUsername("fuck_my_life");
+		account.setPassword("12345");
+		account.setEmail("fml@gmail.com");
+		accountRepository.save(account);
+	}
+	
+	@Test
+	public void test3Account() throws Exception{
+		Account account = new Account();
+		account.setUsername("chrisna2");
+		account.setPassword("pass");
+		account.setEmail("chrisna2@bans.com");
+		Account newAcct = accountRepository.save(account);
+		log.info(newAcct.getId()+" / "+newAcct.getUsername());
+		assertThat(newAcct).isNotNull();
+		
+		Account existAcct = accountRepository.findByUsername(newAcct.getUsername());
+		assertThat(existAcct).isNotNull();
+		
+		Account notExistAcct = accountRepository.findByUsername("test");
+		assertThat(notExistAcct).isNull();
+		
+	}
+	
+	@Test
+	public void test4Read() {
+		accountRepository.findById(1L).ifPresent(
+					account -> log.info(account.toString()));
+	}
+	
+	@Test
+	public void test5Update() {
+		log.info("Read First ................ ");
+		Optional<Account> account = accountRepository.findById(1L);
+		
+		log.info("Update name ............... ");
+		account.get().setUsername("the_final_winner");
+		account.get().setPassword("illbewinner");
+		account.get().setEmail("tfw@gmail.com");
+		
+		log.info("Call save ..............");
+		accountRepository.save(account.get());
+		
+	}
+	
+	@Test
+	public void test6Delete() {
+		log.info("Delete ................ ");
+		accountRepository.deleteById(2L);
+		accountRepository.deleteById(1L);
+	}
+	
+	
 }
