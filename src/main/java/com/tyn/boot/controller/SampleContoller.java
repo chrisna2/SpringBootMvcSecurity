4470e273f4ede9633cf3be4efd63f071dcaa2911
chrisna2 <marksman703@gmail.com>
2019-11-12 10:09:54 +0900

    sample6 까지 타입리프

--- src/main/java/com/tyn/boot/controller/SampleContoller.java
+++ src/main/java/com/tyn/boot/controller/SampleContoller.java
@@ -35,5 +35,36 @@ public class SampleContoller{
         model.addAttribute("list", list);
     }   
 
+    @GetMapping("/sample4")
+    public void sample4(Model model){
+        List<MemberVO> list = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            list.add(new MemberVO(i, "u000"+i%3, "p0000"+i%3, "홍길동"+i, new Timestamp(System.currentTimeMillis())));
+        }
+        model.addAttribute("list", list);
+    }   
+
+    @GetMapping("/sample5")
+    public void sample5(Model model){
+        String result = "SUCCESS";
+        model.addAttribute("result", result);
+    }   
+
+    @GetMapping("/sample6")
+    public void sample6(Model model){
+        List<MemberVO> list = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++){
+            list.add(new MemberVO(i, "u0"+i, "p0"+i, "홍길동"+i, new Timestamp(System.currentTimeMillis())));
+        }
+
+        model.addAttribute("list", list);
+
+        String result = "SUCCESS";
+
+        model.addAttribute("result", result);
+    }   
+
+
 
 }
--- src/main/java/com/tyn/boot/controller/TynController.java
+++ src/main/java/com/tyn/boot/controller/TynController.java
@@ -17,5 +17,4 @@ public class TynController {
 		return "hello_"+service.getName();
 	}
 	
-
 }
